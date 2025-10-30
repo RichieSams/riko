ffa18885f5b8c15c897e15bfc6709beb8324c4f5
RichieSams <adastley@gmail.com>
2025-10-30 17:21:19 -0400

    Add a haproxy endpoint for vmagent

--- renew-certs.sh
+++ renew-certs.sh
@@ -11,3 +11,9 @@ certbot certonly --non-interactive --agree-tos \
     --authenticator dns-porkbun --dns-porkbun-credentials /conf/porkbun.ini --dns-porkbun-propagation-seconds 60 \
     --renew-hook "/bin/bash /reload-haproxy.sh grafana.astleymanor.net" \
     -d "grafana.astleymanor.net"
+
+certbot certonly --non-interactive --agree-tos \
+    --email adastley@gmail.com --preferred-challenges dns \
+    --authenticator dns-porkbun --dns-porkbun-credentials /conf/porkbun.ini --dns-porkbun-propagation-seconds 60 \
+    --renew-hook "/bin/bash /reload-haproxy.sh vmagent.astleymanor.net" \
+    -d "vmagent.astleymanor.net"
