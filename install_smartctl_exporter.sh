e498eb7d21e3fa216cd1db98393b97c48c8159f1
RichieSams <adastley@gmail.com>
2025-10-30 17:11:32 -0400

    Add smartctl exporter

---
+++ install_smartctl_exporter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wget https://github.com/prometheus-community/smartctl_exporter/releases/download/v0.14.0/smartctl_exporter-0.14.0.linux-amd64.tar.gz
+
+tar xvfz smartctl_exporter-*.*-amd64.tar.gz
+sudo chown root: smartctl_exporter-*.*-amd64/smartctl_exporter
+sudo mv smartctl_exporter-*.*-amd64/smartctl_exporter /usr/local/bin/
+
+sudo rm -rf smartctl_exporter-*.*-amd64/smartctl_exporter*
+
+sudo cp smartctl_exporter.service /etc/systemd/system/smartctl_exporter.service
+sudo chown root: /etc/systemd/system/smartctl_exporter.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable smartctl_exporter.service
+sudo systemctl start smartctl_exporter.service
